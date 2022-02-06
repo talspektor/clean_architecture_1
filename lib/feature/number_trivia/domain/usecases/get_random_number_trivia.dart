97becc1b1992cfef75794cf8928650336de1d287
Tal Spektor <tal.s@zemingo.com>
2022-02-06 20:31:11 +0200

    Bloc wip

--- lib/feature/number_trivia/domain/usecases/get_random_number_trivia.dart
+++ lib/feature/number_trivia/domain/usecases/get_random_number_trivia.dart
@@ -3,7 +3,6 @@ import 'package:clean_architecture_1/core/usecases/usecase.dart';
 import 'package:clean_architecture_1/feature/number_trivia/domain/entities/number_trivia.dart';
 import 'package:clean_architecture_1/feature/number_trivia/domain/repositories/number_trivia_repository.dart';
 import 'package:dartz/dartz.dart';
-import 'package:equatable/equatable.dart';
 
 class GetNumberRandomTrivia implements UseCase<NumberTrivia, NoParams> {
   final NumberTriviaRepository repository;
--- lib/feature/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ lib/feature/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -1,7 +1,11 @@
 import 'package:clean_architecture_1/core/error/failures.dart';
+import 'package:clean_architecture_1/core/usecases/usecase.dart';
 import 'package:clean_architecture_1/core/util/input_converter.dart';
+import 'package:clean_architecture_1/feature/number_trivia/domain/entities/number_trivia.dart';
 import 'package:clean_architecture_1/feature/number_trivia/domain/usecases/get_concrete_number_tivia.dart';
+import 'package:clean_architecture_1/feature/number_trivia/domain/usecases/get_random_number_trivia.dart';
 import 'package:clean_architecture_1/feature/number_trivia/presentation/bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 const String serverFailureMessage = 'Server Failure';
@@ -11,30 +15,49 @@ const String invalidInputFailure =
 
 class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
   final GetConcreteNumberTrivia getConcreteNumberTrivia;
-  final GetTriviaFromRandomNumber getTriviaFromRandomNumber;
+  final GetNumberRandomTrivia getNumberRandomTrivia;
   final InputConverter inputConverter;
 
   NumberTriviaBloc({
     required this.getConcreteNumberTrivia,
-    required this.getTriviaFromRandomNumber,
+    required this.getNumberRandomTrivia,
     required this.inputConverter,
   }) : super(Empty()) {
     on<GetTriviaForConcreteNumber>((event, emit) {
-      final inputEither =
-          inputConverter.stringToUnsignedInteger(event.numberString);
-      inputEither.fold((failure) {
-        emit(Error(message: invalidInputFailure));
-      }, (integer) async {
-        emit(Loading());
-        final failureOrTrivia =
-            await getConcreteNumberTrivia(Params(number: integer));
-        failureOrTrivia.fold(
-            (failure) => emit(Error(message: _mapFailureToMessage(failure))),
-            (trivia) => emit(Loaded(trivia: trivia)));
-      });
+      _onGetTriviaForConcreteNumber(event, emit);
     });
 
-    on<GetTriviaFromRandomNumber>((event, emit) {});
+    on<GetTriviaFromRandomNumber>((event, emit) async {
+      _onGetTriviaFromRandomNumber(event, emit);
+    });
+  }
+
+  _onGetTriviaForConcreteNumber(
+      GetTriviaForConcreteNumber event, Emitter<NumberTriviaState> emit) {
+    final inputEither =
+        inputConverter.stringToUnsignedInteger(event.numberString);
+    inputEither.fold((failure) {
+      emit(Error(message: invalidInputFailure));
+    }, (integer) async {
+      emit(Loading());
+      final failureOrTrivia =
+          await getConcreteNumberTrivia(Params(number: integer));
+      _eitherLoadedOrErrorState(failureOrTrivia, emit);
+    });
+  }
+
+  _onGetTriviaFromRandomNumber(
+      GetTriviaFromRandomNumber event, Emitter<NumberTriviaState> emit) async {
+    emit(Loading());
+    final failureOrTrivia = await getNumberRandomTrivia(NoParams());
+    _eitherLoadedOrErrorState(failureOrTrivia, emit);
+  }
+
+  _eitherLoadedOrErrorState(Either<Failure, NumberTrivia> failureOrTrivia,
+      Emitter<NumberTriviaState> emit) {
+    failureOrTrivia.fold(
+        (failure) => emit(Error(message: _mapFailureToMessage(failure))),
+        (trivia) => emit(Loaded(trivia: trivia)));
   }
 
   String _mapFailureToMessage(Failure failure) {
